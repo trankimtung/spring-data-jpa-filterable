53dce182b80592e9ea9d016a09d35f24bc6ca9cf
Tran Kim Tung <contact@trankimtung.com>
2021-09-02 00:40:55 +0800

    Adds publish to maven workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -99,6 +99,9 @@ publishing {
 }
 
 signing {
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
     sign(publishing.publications["mavenJava"])
 }
 
